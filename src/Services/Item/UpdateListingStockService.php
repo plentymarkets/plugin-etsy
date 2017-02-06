7271eac483ae1e27185273c6dbbf2aed68e32bbb
Tudor-Dan Ravoiu <tudor.ravoiu@plentymarkets.com>
2017-02-06 12:37:56 +0100

    FIX price format for update

--- src/Services/Item/UpdateListingStockService.php
+++ src/Services/Item/UpdateListingStockService.php
@@ -59,7 +59,7 @@ class UpdateListingStockService
 					'listing_id' => (int) $listingId,
 					'state'      => $this->isVariationAvailable($record) ? ListingService::STATE_ACTIVE : ListingService::STATE_INACTIVE,
 					'quantity'   => $this->itemHelper->getStock($record) > 0 ? $this->itemHelper->getStock($record) : 1,
-					'price'      => $record->variationRetailPrice->price,
+					'price'      => number_format($record->variationRetailPrice->price, 2),
 				];
 
 				$this->listingService->updateListing($listingId, $data);
