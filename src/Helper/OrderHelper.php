e9ac434e50888a8bb20e67601a159a328eaf2704
Tudor-Dan Ravoiu <tudor.ravoiu@plentymarkets.com>
2017-02-09 13:37:16 +0100

    FIX minor issue

--- src/Helper/OrderHelper.php
+++ src/Helper/OrderHelper.php
@@ -234,8 +234,8 @@ class OrderHelper
 		/** @var OrderRepositoryContract $orderRepo */
 		$orderRepo = pluginApp(OrderRepositoryContract::class);
 
-		if($orderRepo instanceof OrderRepositoryContract)
-		{
+		// if($orderRepo instanceof OrderRepositoryContract) TODO uncomment this as soon as setFilters is available
+		// {
 			$orderRepo->setFilters([
 				                       'externalOrderId' => $externalOrderId,
 				                       'referrerId'      => $this->getReferrerId(),
@@ -252,7 +252,7 @@ class OrderHelper
 					return true;
 				}
 			}
-		}
+		// }
 
 		return false;
 	}
