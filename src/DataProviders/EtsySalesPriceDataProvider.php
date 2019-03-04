294aa0f9bcb5d55bd7a20427497d1266c0d2b41b
Sebastian Käßinger <sebastian.kaessinger@plentymarkets.com>
2019-03-04 11:59:40 +0100

    added translations for sales price data provider

--- src/DataProviders/EtsySalesPriceDataProvider.php
+++ src/DataProviders/EtsySalesPriceDataProvider.php
@@ -2,10 +2,26 @@
 
 namespace Etsy\DataProviders;
 
+use Etsy\EtsyServiceProvider;
 use Plenty\Modules\Catalog\DataProviders\BaseDataProvider;
+use Plenty\Plugin\Translation\Translator;
 
 class EtsySalesPriceDataProvider  extends BaseDataProvider
 {
+    /**
+     * @var Translator
+     */
+    protected $translator;
+
+    /**
+     * EtsySalesPriceDataProvider constructor.
+     * @param Translator $translator
+     */
+    public function __construct(Translator $translator)
+    {
+        $this->translator = $translator;
+    }
+
     /**
      * @inheritdoc
      */
@@ -14,12 +30,12 @@ class EtsySalesPriceDataProvider  extends BaseDataProvider
         return [
             [
                 'key' => 'sales_price',
-                'label' => 'sales price',
+                'label' => $this->translator->trans(EtsyServiceProvider::PLUGIN_NAME.'::catalog.sales_price'),
                 'required' => true,
             ],
             [
                 'key' => 'currency',
-                'label' => 'currency',
+                'label' => $this->translator->trans(EtsyServiceProvider::PLUGIN_NAME.'::catalog.currency'),
                 'required' => true,
             ]
         ];
