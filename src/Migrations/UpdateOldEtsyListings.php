b7767c70b22f09897f65770141675218a42bf9f7
Sebastian Käßinger <sebastian.kaessinger@plentymarkets.com>
2019-05-07 08:24:35 +0200

    added migration logic

--- src/Migrations/UpdateOldEtsyListings.php
+++ src/Migrations/UpdateOldEtsyListings.php
@@ -3,7 +3,11 @@
 namespace Etsy\Migrations;
 
 
+use Etsy\Api\Services\ListingService;
 use Etsy\Helper\SettingsHelper;
+use Plenty\Modules\Item\VariationMarket\Repositories\VariationMarketRepository;
+use Plenty\Modules\Item\VariationSku\Contracts\VariationSkuRepositoryContract;
+use Plenty\Modules\Item\VariationSku\Repositories\VariationSkuRepository;
 
 class UpdateOldEtsyListings
 {
@@ -11,8 +15,36 @@ class UpdateOldEtsyListings
     {
         /** @var SettingsHelper $settingsHelper */
         $settingsHelper = pluginApp(SettingsHelper::class);
+        /** @var VariationSkuRepository $variationSkuRepository */
+        $variationSkuRepository = pluginApp(VariationSkuRepositoryContract::class);
+        /** @var ListingService $listingService */
+        $listingService = pluginApp(ListingService::class);
 
-        // todo check if listing is online if yes -> delete if no-> continue
-        // todo delete entry in variation_market_status where marketId = Etsy to delete the old etsy sku´s 
+        try {
+            $marketId = $settingsHelper->get(SettingsHelper::SETTINGS_ORDER_REFERRER);
+
+            $filter = [
+                'marketId' => $marketId
+            ];
+
+            $listings = $variationSkuRepository->search($filter);
+
+            foreach ($listings as $listing) {
+                // etsy listing id
+                $listingId = $listing->sku;
+                // primary key from plenty_item_variation_market_status
+                $tableId = $listing->id;
+
+                $response = $listingService->getListing($listingId);
+
+                if ($response['results'][0]['state'] !== "removed") {
+                    $listingService->deleteListing($listingId);
+                }
+
+                $variationSkuRepository->delete($tableId);
+            }
+        } catch (\Exception $exception) {
+            $exception->getMessage();
+        }
     }
 }
