6353d87e40649fdbe54123184e38f0ed0b7cc67e
Sebastian Käßinger <sebastian.kaessinger@plentymarkets.com>
2019-05-08 08:36:41 +0200

    updated plugin.json code clean up

--- src/Services/Batch/Item/ItemExportService.php
+++ src/Services/Batch/Item/ItemExportService.php
@@ -82,9 +82,8 @@ class ItemExportService extends AbstractBatchService
 
         foreach ($catalogResult as $variation) {
 
-            if (isset($variation['do_not_export'])){
-                continue;
-            }
+            if (isset($variation['do_not_export'])) continue;
+
 
             //for convenience we get rid of all skus that are not related to Etsy
             $skus = [];
