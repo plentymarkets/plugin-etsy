601811d71ab574fa9c8708bbb08ffbc3c042f7bb
Stefan Cuculeac <stefan.cuculeac@plentymarkets.com>
2022-09-20 12:04:25 +0300

    Added checkbox for ImageFileCleanupCron

--- src/Crons/ImageFileCleanupCron.php
+++ src/Crons/ImageFileCleanupCron.php
@@ -3,7 +3,10 @@
 namespace Etsy\Crons;
 
 
+use Etsy\Helper\SettingsHelper;
 use Plenty\Modules\Plugin\Libs\Contracts\LibraryCallContract;
+use Plenty\Plugin\ConfigRepository;
+
 
 class ImageFileCleanupCron
 {
@@ -14,16 +17,34 @@ class ImageFileCleanupCron
     private $libraryCall;
 
     /**
+     * @var ConfigRepository
+     */
+    private $config;
+
+    /**
      * ImageFileCleanupCron constructor.
      * @param LibraryCallContract $libraryCall
      */
-    public function __construct(LibraryCallContract $libraryCall)
+    public function __construct(LibraryCallContract $libraryCall, ConfigRepository $config)
     {
         $this->libraryCall = $libraryCall;
+        $this->config = $config;
     }
     
     public function handle()
     {
+        if($this->checkIfCanRun() == 'true') return;
+
         $this->libraryCall->call(self::IMAGE_FILE_CLEANUP);
+    }
+
+    /**
+     * Return if we can run this cron or is disabled
+     *
+     * @return bool
+     */
+    private function checkIfCanRun(): bool
+    {
+        return $this->config->get(SettingsHelper::PLUGIN_NAME . '.imageCleanup', 'true');
     }
 }
