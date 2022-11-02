cbcc06963fb6580f2696db6b4697b03295b105c9
Stefan Cuculeac <stefan.cuculeac@plentymarkets.com>
2022-11-02 11:59:21 +0200

    Fix issue with load catalog

--- src/EtsyServiceProvider.php
+++ src/EtsyServiceProvider.php
@@ -81,7 +81,7 @@ class EtsyServiceProvider extends ServiceProvider
         $this->getApplication()->register(EtsyRouteServiceProvider::class);
 
         $configRepository = pluginApp(ConfigRepository::class);
-        if(!$configRepository->get(SettingsHelper::PLUGIN_NAME . '.stockUpdate', false) || !$configRepository->get(SettingsHelper::PLUGIN_NAME . '.listings', false)){
+        if($configRepository->get(SettingsHelper::PLUGIN_NAME . '.stockUpdate', 'false') == 'false' || $configRepository->get(SettingsHelper::PLUGIN_NAME . '.listings', 'false') == 'false'){
             $this->getApplication()->register(CatalogBootServiceProvider::class);
         }
     }
